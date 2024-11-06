03c843ee729b905c06120c54c01c43d016b9e04c
Volodymyr <vladsergmvs@gmail.com>
2024-11-06 18:31:23 +0200

    form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,45 +1,42 @@
 const STORAGE_KEY = 'feedback-form-state',
-  formData = { email: '', message: '' },
-  form = document.querySelector('.feedback-form');
+       formData = { email: '', message: '' },
+       form = document.querySelector('.feedback-form'),
+       inputEmail = document.querySelector('input'),
+       textareaMessage =  document.querySelector('textarea');
   
+
+
+///////////////////////////////////////////////////////////////////
+
+  document.addEventListener('DOMContentLoaded', ()=>{   
+    if( localStorage.getItem(STORAGE_KEY) ?? ""){
+      inputEmail.value = JSON.parse( localStorage.getItem(STORAGE_KEY)).email;
+      textareaMessage.value = JSON.parse( localStorage.getItem(STORAGE_KEY)).message;     
+    }
+       
+  });
+  
+  
+///////////////////////////////////////////////////////////////////
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
+   if( inputEmail.value ==='' ||  textareaMessage.value ===''){
+     alert('Fill please all fields!');
+     return;
+   }
 	 console.log( JSON.parse(localStorage.getItem(STORAGE_KEY)));
    localStorage.removeItem(STORAGE_KEY );
   form.reset();
 
 });
 
+///////////////////////////////////////////////////////////////////
 
 form.addEventListener('input', (event)=>{
   formData[event.target.name] = event.target.value.trim();
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-   
-   
-  
 });
 
 
-
-//////////////////////////////////////////////////////////////////
-
-const populateForm = function (){
-  const savedObject = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-}
-populateForm();
-
-
-// textarea.value = localStorage.getItem(STORAGE_KEY) ?? "";
-
-// form.addEventListener("input", (evt) => {
-//   localStorage.setItem(localStorageKey, evt.target.value);
-// });
-
-// form.addEventListener("submit", (evt) => {
-//   evt.preventDefault();
-// 	console.log(evt.target.elements.message.value);
-//   localStorage.removeItem(localStorageKey);
-//   form.reset();
-// });
 
