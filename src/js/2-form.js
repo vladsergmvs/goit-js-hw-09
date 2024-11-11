3e2a12b4be68edded93be9b75fbd5e7a8c59af85
Volodymyr <vladsergmvs@gmail.com>
2024-11-11 14:02:25 +0200

    formData

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,19 +5,31 @@ const STORAGE_KEY = 'feedback-form-state',
        textareaMessage =  document.querySelector('textarea');
   
 
-
+      //  не виконаний цей пункт ТЗ - При завантаженні сторінки перевір, чи є дані у локальному сховищі.
+      //   Якщо так, використовуй їх для заповнення об'єкта formData.
+      //  не виконаний цей пункт ТЗ - Якщо всі поля заповнені, очисти об’єкт formData .
 ///////////////////////////////////////////////////////////////////
 
   document.addEventListener('DOMContentLoaded', ()=>{   
     if( localStorage.getItem(STORAGE_KEY) ?? ""){
       inputEmail.value = JSON.parse( localStorage.getItem(STORAGE_KEY)).email;
-      textareaMessage.value = JSON.parse( localStorage.getItem(STORAGE_KEY)).message;     
+      textareaMessage.value = JSON.parse( localStorage.getItem(STORAGE_KEY)).message;  
+     formData.email =  inputEmail.value;
+     formData.message = textareaMessage.value;
+   
     }
        
   });
   
   
 ///////////////////////////////////////////////////////////////////
+
+form.addEventListener('input', (event)=>{
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+///////////////////////////////////////////////////////////////////////
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -26,17 +38,13 @@ form.addEventListener('submit', (event) => {
      return;
    }
 	 console.log( JSON.parse(localStorage.getItem(STORAGE_KEY)));
+   formData.email ='';
+   formData.message ='';
    localStorage.removeItem(STORAGE_KEY );
   form.reset();
 
 });
 
 ///////////////////////////////////////////////////////////////////
-
-form.addEventListener('input', (event)=>{
-  formData[event.target.name] = event.target.value.trim();
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-});
-
 
 
